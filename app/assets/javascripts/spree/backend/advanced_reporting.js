d3adcd9df77b8eeb780a9eac985b46a3d0a58127
Dang Hieu <danghieu193@gmail.com>
2016-04-14 16:37:58 +0700

    manage report

---
+++ app/assets/javascripts/spree/backend/advanced_reporting.js
@@ -0,0 +1,36 @@
+$(function() {
+	$('ul#show_data li').click(function() {
+		$('ul#show_data li').not(this).removeClass('selected');
+		$(this).addClass('selected');
+		var id = 'div#' + $(this).attr('id') + '_data';
+		$('div.advanced_reporting_data').not($(id)).hide();
+		$(id).show(); 
+	});
+	$('table.tablesorter').tablesorter();
+	$('table.tablesorter').bind("sortEnd", function() {
+		var section = $(this).parent().attr('id');
+		var even = true;
+		$.each($('div#' + section + ' table tr'), function(i, j) {
+			$(j).removeClass('even').removeClass('odd');
+			$(j).addClass(even ? 'even' : 'odd');
+			even = !even;
+		});
+	});
+	if($('input#product_id').length > 0) {
+		$('select#advanced_reporting_product_id').val($('input#product_id').val());
+	}
+	if($('input#taxon_id').length > 0) {
+		$('select#advanced_reporting_taxon_id').val($('input#taxon_id').val());
+	}
+	$('div#advanced_report_search form').submit(function() {
+		$('div#advanced_report_search form').attr('action', $('select#report').val());
+	});
+
+	if(completed_at_gt != '') {
+		$('input#search_completed_at_gt').val(completed_at_gt);
+	}
+	if(completed_at_lt != '') {
+		$('input#search_completed_at_lt').val(completed_at_lt);
+	}
+})
+	
---
+++ lib/spree/advanced_report/top_report/top_customers.rb
@@ -0,0 +1,34 @@
+class Spree::AdvancedReport::TopReport::TopCustomers < Spree::AdvancedReport::TopReport
+  def name
+    "Top Customers"
+  end
+
+  def description
+    "Top purchasing customers, calculated by revenue"
+  end
+
+  def initialize(params, limit)
+    super(params)
+
+    orders.each do |order|
+      if order.user
+        data[order.user.id] ||= {
+          :email => order.user.email,
+          :revenue => 0,
+          :units => 0
+        }
+        data[order.user.id][:revenue] += revenue(order)
+        data[order.user.id][:units] += units(order)
+      end
+    end
+
+    self.ruportdata = Table(%w[email Units Revenue])
+    data.inject({}) { |h, (k, v) | h[k] = v[:revenue]; h }.sort { |a, b| a[1] <=> b [1] }.reverse[0..limit].each do |k, v|
+      ruportdata << { "email" => data[k][:email], "Units" => data[k][:units], "Revenue" => data[k][:revenue] } 
+    end 
+    ruportdata.replace_column("Revenue") { |r| "%0.0f VND" % r.Revenue }
+    ruportdata.rename_column("email", "Customer Email")
+  end
+end
+
+#<%= number_to_currency(product.price, :unit => product.currency, precision: 0, seperator: ".", format: "%n %u") rescue '' %>
